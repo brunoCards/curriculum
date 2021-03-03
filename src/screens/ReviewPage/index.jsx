db7a913fedfdaee31a1a841777617bfe3ccc484d
Bruno Cardoso <bruno.cardoso@fengbrasil.com.br>
2021-03-03 08:48:11 -0300

    commit de limpeza

---
+++ src/screens/ReviewPage/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//import context
+import { useFormulary } from '../../contexts/FormContext';
+
+//components
+import Header from '../../components/Header';
+
+//styled-components
+import { PageLayout } from './styles';
+
+const ReviewPage = () => {
+  const { form } = useFormulary();
+  return (
+    <>
+      <Header />
+      <PageLayout></PageLayout>
+    </>
+  );
+};
+
+export default ReviewPage;
